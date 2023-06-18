10aaf8c728501e888f8be82ab130c9d4da020287
Diogo Jose <diogospk@hotmail.com>
2023-06-18 00:22:43 -0300

    first commit

---
+++ 01modalidades_enum.dart
@@ -0,0 +1,61 @@
+import 'dart:io';
+
+void main() {
+  print("Digite o CÓDIGO da modalidade que deseja se inscrever: \n"
+      "1 - Futebol\n"
+      "2 - Ginástica\n"
+      "3 - Natação\n"
+      "4 - Atletismo");
+  String? inputCodModalidade = stdin.readLineSync();
+  int codModalidade = int.parse(inputCodModalidade!);
+
+  String escolha;
+  if (codModalidade == 1) {
+    escolherModalidadeEnum(Modalidades.futebol);
+    Modalidades modalidade = Modalidades.futebol;
+    print(modalidade);
+
+  } else if (codModalidade == 2) {
+    Modalidades modalidade = Modalidades.ginastica;
+    print(modalidade);
+    escolherModalidadeEnum(Modalidades.ginastica);
+
+  } else if (codModalidade == 3) {
+    Modalidades modalidade = Modalidades.natacao;
+    print(modalidade);
+    escolherModalidadeEnum(Modalidades.natacao);
+    
+  } else if (codModalidade == 4) {
+    escolherModalidadeEnum(Modalidades.atletismo);
+    Modalidades modalidade = Modalidades.atletismo;
+    print(modalidade);
+    escolha = "Atletismo";
+
+  } else {
+    print("Opção inválida");
+    escolha = "Opção Inválida";
+  }
+
+}
+
+
+enum Modalidades { futebol, ginastica, natacao, atletismo }
+
+void escolherModalidadeEnum(Modalidades escolha) {
+  switch (escolha) {
+    case Modalidades.futebol:
+      print("A modalidade selecionada foi Futebol");
+      break;
+    case Modalidades.ginastica:
+      print("A modalidade selecionada foi Ginastica");
+      break;
+    case Modalidades.natacao:
+      print("A modalidade selecionada foi Natação");
+      break;
+    case Modalidades.atletismo:
+      print("A modalidade selecionada foi Atletismo");
+      break;
+    default:
+      print("Opção inválida");
+  }
+}
