86ddad7cedd3a132967b47d56b878bc30ac9b550
Diogo Jose <diogospk@hotmail.com>
2023-06-20 20:33:32 -0300

    finalizando o enum

--- 02treinando_enum.dart
+++ 02treinando_enum.dart
@@ -1,7 +1,5 @@
-
-
 void main() {
-  escolherConsoleEnum(Console.nintendinho);
+  escolherConsoleEnum(Console.na);
 }
 
 enum Console {
@@ -30,7 +28,27 @@ void escolherConsoleEnum(Console videogame) {
       print(
           "Nintendo64 também conhecido como 64, foi lançado em 1996 no Japão, Estados Unidos e Brasil");
       break;
+
+    case Console.gameCube:
+      print(
+          "O Nintendo GameCube foi lançado em 2001 no Japão e em 2002 no Brasil");
+      break;
+
+    case Console.nintendoWii:
+      print(
+          "O Nintendo Wii foi lançado em 2006 Tanto no Japão quanto no Brasil");
+      break;
+
+    case Console.nintendoWiiU:
+      print(
+          "Nintendo WiiU foi lançado em 2012 no Japão e chegou em 2013 no Brasil");
+      break;
+
+    case Console.nintendoSwitch:
+      print(
+          "O Nintendo Switch foi lançado em 2017 no Japão e apenas em 2020 no Brasil");
+      break;
     default:
-      print("Não escolheu nenhum console");
+      print("Prefere PC Gamer");
   }
 }
